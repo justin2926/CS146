bb7701430a84ce052f58248bb1eb2600006fc52f
Justin <32648817+justin2926@users.noreply.github.com>
2024-02-07 21:59:41 -0800

    zzz

--- HW/HW2/Main.java
+++ HW/HW2/Main.java
@@ -1,9 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(firstBadVersion(4));
     }
 
     public static boolean isBadVersion(int x) {
-        return true;
+        return x == 4 || x == 5 || x == 6;
+    }
+
+    public static int firstBadVersion(int n) {
+        int first = 1;
+        int last = n;
+
+        while (first < last) {
+            int mid = (first + last) / 2;
+            if (isBadVersion(mid)) {
+                last = mid;
+            } else {
+                first = mid+1;
+            }
+        }
+        return first;
     }
 }
