26596f69bcfeae786df9fa8f341c78319316cff2
Justin <32648817+justin2926@users.noreply.github.com>
2024-02-21 08:45:52 -0800

    Update

--- HW/HW6/Main.java
+++ HW/HW6/Main.java
@@ -4,22 +4,9 @@ public class Main {
     }
 
     public List<List<Integer>> threeSum(int[] nums) {
-
-        List<Integer> l = new ArrayList<Integer>();
-
-       for (int i = 0; i < nums[i]; i++) {
-           for (int j = i + 1; i j < nums[j]; j++) {
-               for (int k = j + 1; k < nums[k]; k++) {
-                   if (nums[i] + nums[j] + nums[k] == 0) {
-                       l.add(i);
-                       l.add(j);
-                       l.add(k);
-                   }
-               }
-           }
-       }
-
+        return null;
     }
 
-    return l.;
+
+
 }
