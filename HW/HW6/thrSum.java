579ffae10dd2cb663b6220ba6a63b94d4b1f6f80
Justin <32648817+justin2926@users.noreply.github.com>
2024-02-21 20:27:05 -0800

    Update

--- HW/HW6/thrSum.java
+++ HW/HW6/thrSum.java
@@ -3,11 +3,29 @@ import java.util.Arrays;
 public class thrSum {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> n = new ArrayList<>();
-
         Arrays.sort(nums);
-
-
-
+        for (int i = 0; i < nums.length-2; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length-1; j++) {
+                if (j > i + 1 && nums[j] == nums[j-1]) {
+                    continue;
+                }
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (k > j + 1 && nums[k] == nums[k-1]) {
+                        continue;
+                    }
+                    if (nums[i] + nums[j] + nums[k] == 0 && (i != j && i != k && j != k)) {
+                        List<Integer> m = new ArrayList<>(3);
+                        m.add(nums[i]);
+                        m.add(nums[j]);
+                        m.add(nums[k]);
+                        n.add(m);
+                    }
+                }
+            }
+        }
         return n;
     }
 }
