e5acb23f0f06a117255bd284b32d31897b573307
Justin <nguyenjustin581@gmail.com>
2024-02-23 13:00:04 -0800

    Update

--- HW/HW7/MeetingRooms.java
+++ HW/HW7/MeetingRooms.java
@@ -1,13 +1,39 @@
+import java.util.Arrays;
+
 public class MeetingRooms {
     public static void main(String[] args) {
         MeetingRooms m = new MeetingRooms();
 
-        int[][] a = { {0, 30}, {5, 10}, {15, 20}};
+        int[][] a = {   {0, 30},
+                        {5, 10},
+                        {15, 20}
+                    };
 
-        System.out.println(m.minMeetingRooms(a));;
+        int[][] b = {   {0, 1},
+                        {1, 2},
+                        {2, 3}
+                    };
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.println("a[" + i + "][" + j + "] = "
+                        + a[i][j]);
+            }
+        }
+
+//        System.out.println(m.minMeetingRooms(a));
+//        System.out.println(m.minMeetingRooms(b));
     }
 
     public int minMeetingRooms(int[][] intervals) {
-        return intervals.length;
+        Arrays.sort(intervals);
+        int min = 0;
+
+//        for (int i = 0; i < intervals.length; i++) {
+//            for (int j = 0; j < intervals[i].length; j++) {
+//
+//            }
+//        }
+        return 0;
     }
 }
