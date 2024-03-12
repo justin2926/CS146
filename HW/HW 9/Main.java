7d6678afb0d6fe52e92e141e626c65441f901b76
Justin <32648817+justin2926@users.noreply.github.com>
2024-03-11 19:29:38 -0700

    Update Main.java

--- HW/HW 9/Main.java
+++ HW/HW 9/Main.java
@@ -1,1 +1,19 @@
+public class Main {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode l = lowestCommonAncestor(root.left, p, q);
+        TreeNode r = lowestCommonAncestor(root.right, p, q);
+
+        if (l == null) {
+            return r;
+        }
+        if (r == null) {
+            return l;
+        }
+        return root;
+    }
+}
 
