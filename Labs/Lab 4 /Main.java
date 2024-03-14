1142ac51d827d36e4c925ccd13ecb6cf800cffdd
Justin <32648817+justin2926@users.noreply.github.com>
2024-03-13 22:12:23 -0700

    Update Main.java

--- Labs/Lab 4 /Main.java
+++ Labs/Lab 4 /Main.java
@@ -1,1 +1,14 @@
+public TreeNode invertTree(TreeNode root) {
+    if (root == null) {
+        return root;
+    }
 
+    TreeNode temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+
+    invertTree(root.left);
+    invertTree(root.right);
+
+    return root;
+}
