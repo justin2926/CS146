84a174a3fc10e3415629a3a3a6c278e33493ea35
Justin <32648817+justin2926@users.noreply.github.com>
2024-03-13 23:24:04 -0700

    Update Main.py

--- Labs/Lab 5 /Main.py
+++ Labs/Lab 5 /Main.py
@@ -1,1 +1,13 @@
+def isValidBST(self, root: Optional[TreeNode]) -> bool:
+    def validBST(root, minVal=float('-inf'), maxVal=float('inf')):
+        if root is None:
+            return True
 
+        if not (minVal < root.val < maxVal):
+            return False
+
+        left = validBST(root.left , minVal, root.val)
+        right = validBST(root.right, root.val, maxVal)
+
+        return left and right
+    return validBST(root)
