8c678205b7bd79ba25b533cee6f2377bd3f97c16
Justin <32648817+justin2926@users.noreply.github.com>
2024-03-13 22:15:31 -0700

    Update Main.py

--- Labs/Lab 4 /Main.py
+++ Labs/Lab 4 /Main.py
@@ -1,1 +1,12 @@
+def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+    if root is None:
+        return root
 
+    temp = root.left
+    root.left = root.right
+    root.right = temp
+
+    self.invertTree(root.left)
+    self.invertTree(root.right)
+
+    return root
