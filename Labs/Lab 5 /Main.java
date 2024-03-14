4dc7d3bfcec7f20b1389b8369b92390a44232c74
Justin <32648817+justin2926@users.noreply.github.com>
2024-03-13 23:23:47 -0700

    Update Main.java

--- Labs/Lab 5 /Main.java
+++ Labs/Lab 5 /Main.java
@@ -1,1 +1,16 @@
+public boolean isValidBST(TreeNode root) {
+        return isValidBST(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
 
+public boolean isValidBST(TreeNode root,long minVal,long maxVal) {
+    if (root == null)
+        return true;
+
+    if (root.val <= minVal || root.val >= maxVal)
+        return false;
+    
+    boolean left = isValidBST(root.left,minVal,root.val);
+    boolean right = isValidBST(root.right,root.val,maxVal);
+
+    return left && right;
+}
