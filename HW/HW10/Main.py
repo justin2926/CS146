aaafa472fff8c6e927b6b5258d241a24beac4305
Justin <32648817+justin2926@users.noreply.github.com>
2024-03-16 22:05:59 -0700

    Update Main.py

--- HW/HW10/Main.py
+++ HW/HW10/Main.py
@@ -1,1 +1,18 @@
+def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+    ans = []
 
+    q = collections.deque()
+    q.append(root)
+
+    while q:
+        length = len(q)
+        level = []
+        for i in range(length):
+            n = q.popleft()
+            if n:
+                level.append(n.val)
+                q.append(n.left)
+                q.append(n.right)
+        if level:
+            ans.append(level)
+    return ans
