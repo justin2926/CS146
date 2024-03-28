646463c3c01e7184d53bfb5cb88ff8da31ceb2c8
Justin <32648817+justin2926@users.noreply.github.com>
2024-03-27 22:28:09 -0700

    Update Main.java

--- Labs/Lab 6/Main.java
+++ Labs/Lab 6/Main.java
@@ -1,1 +1,50 @@
+public boolean canFinish(int numCourses, int[][] prerequisites) {
+    int size = prerequisites.length;
 
+    ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+    for(int i = 0; i < numCourses ;i++) {
+        list.add(new ArrayList<>());
+    }
+
+    for(int i = 0; i < size; i++) {
+        list.get(prerequisites[i][0]).add(prerequisites[i][1]);
+    }
+
+    int[] inDegree = new int[numCourses];
+  
+    for(int i = 0; i < numCourses; i++) {
+        for(int x: list.get(i)){
+            inDegree[x]++;
+        }
+    }
+
+    Queue<Integer> queue = new LinkedList<>();
+    for(int i = 0; i < numCourses; i++) {
+        if (inDegree[i]==0) {
+            queue.add(i);
+        } 
+    }
+
+    ArrayList<Integer> result = new ArrayList<>();
+
+    while (!queue.isEmpty()) {
+        int n = queue.poll();
+
+        result.add(n);
+        
+        for(int i: list.get(n)) {
+            inDegree[i]--;
+
+            if (a[i] == 0) {
+                queue.add(i);
+            }
+        }
+    }
+
+    if (result.size() != numCourses) {
+        return false;
+    }
+
+    return true;
+}
