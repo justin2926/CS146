26dc8e2422acf8465051812caa71a22df32a12d9
Justin <32648817+justin2926@users.noreply.github.com>
2024-04-17 23:03:11 -0700

    Update Main.py

--- Labs/Lab 7/Main.py
+++ Labs/Lab 7/Main.py
@@ -1,1 +1,37 @@
-
+def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+    def bfs(graph, s, k):
+        q = [(0, s)]  # (distance, node)
+        visited = [False] * len(graph)
+        node_count = -1
+        
+        while q:
+            distance, node = heapq.heappop(q)
+            
+            if visited[node]:
+                continue
+            
+            visited[node] = True
+            node_count += 1
+            
+            for neighbor, weight in graph[node]:
+                if not visited[neighbor] and weight + distance <= k:
+                    heapq.heappush(q, (weight + distance, neighbor))
+        
+        return node_count
+    
+    graph = [[] for _ in range(n)]
+    
+    for e in edges:
+        graph[e[0]].append((e[1], e[2]))
+        graph[e[1]].append((e[0], e[2]))
+    
+    city = 0
+    min_count = n + 1
+    
+    for i in range(n):
+        count = bfs(graph, i, distanceThreshold)
+        if count <= min_count:
+            city = i
+            min_count = count
+    
+    return city
