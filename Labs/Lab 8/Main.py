f06a83abde7aaacf39133d516891abce0fc5679d
Justin <32648817+justin2926@users.noreply.github.com>
2024-05-04 00:40:23 -0700

    Update Main.py

--- Labs/Lab 8/Main.py
+++ Labs/Lab 8/Main.py
@@ -1,1 +1,13 @@
+def coinChange(self, coins: List[int], amount: int) -> int:
+  d = [amount + 1] * (amount + 1)
+  d[0] = 0
 
+  for a in range(1, amount+1):
+      for c in coins:
+          if a-c >= 0:
+              d[a] = min(d[a], 1 + d[a-c])
+
+  if d[amount] != amount + 1:
+      return d[amount]
+  else:
+      return -1
